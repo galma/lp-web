8938c560f11873cb186aee7d082aea931557256a
Guido Alma <guido.alma@live.com.ar>
2023-06-04 14:15:29 -0300

    landing

--- src/pages/landing/Landing.tsx
+++ src/pages/landing/Landing.tsx
@@ -1,8 +1,25 @@
-const Landing = () => (
-  <div>
-    <h1>LANDING</h1>
-    <a href="/login">LOGIN</a>
-  </div>
-);
+import React from "react";
+
+const Landing: React.FC = () => {
+  return (
+    <div className="flex justify-center items-center h-screen bg-gray-100">
+      <div className="max-w-md w-full space-y-8 p-8 bg-white rounded-lg shadow">
+        <h2 className="text-center text-2xl font-bold">
+          Welcome to LP Challenge
+        </h2>
+        <div>
+          <button className="w-full bg-blue-500 hover:bg-blue-600 text-white font-bold py-2 px-4 rounded">
+            Sign In
+          </button>
+        </div>
+        <div>
+          <button className="w-full bg-gray-200 hover:bg-gray-300 text-gray-700 font-bold py-2 px-4 rounded">
+            Sign Up
+          </button>
+        </div>
+      </div>
+    </div>
+  );
+};
 
 export default Landing;
